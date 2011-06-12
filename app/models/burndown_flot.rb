20ac3e24a4f0fcdd4f59af74d5c4f3f280df8395
Michael Cowden <mikecowden@razoo.com>
2011-06-11 20:48:36 -0400

    make sure the charts allow for the extra day

--- app/models/burndown_flot.rb
+++ app/models/burndown_flot.rb
@@ -20,7 +20,7 @@ class BurndownFlot < Flot
   
   def initialize(canvas = nil,  &block)
     @options ||= {}
-    xaxis :min => 0, :ticks => Burndown.day_labels, :max => 9
+    xaxis :min => 0, :ticks => Burndown.day_labels, :max => 10
     yaxis :min => 0, :tickDecimals => 0
     grid :hoverable => true
     super(canvas, {}, &block)
