bf2a51c8baf8edaf67406c9b8d029edc7b0452d2
Michael Cowden <mikecowden@razoo.com>
2011-06-15 11:11:32 -0400

    allow deletion of defects too

--- app/controllers/defects_controller.rb
+++ app/controllers/defects_controller.rb
@@ -1,9 +1,15 @@
 class DefectsController < RedmineScrumController
   unloadable
-
+  
   def update
-    defect = Defect.find(params[:defect][:id])
-    defect.update_attribute(:description, params[:defect][:description])
-    redirect_to defect.issue
+    @defect = Defect.find(params[:defect][:id])
+    @defect.update_attribute(:description, params[:defect][:description])
+    redirect_to @defect.issue
+  end
+  
+  def destroy
+    @defect = Defect.find(params[:id])
+    @defect.destroy
+    redirect_to @defect.issue
   end
 end
