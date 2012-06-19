6ab62ec6d581ca9cd4710de75c3396be7ca5d414
Michael Cowden <mikecowden@razoo.com>
2012-06-19 14:35:08 -0400

    routes and linking in charts

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ ActionController::Routing::Routes.draw do |map|
     project.resources :backlog, :collection => { :sort => :post, :upcoming => :get }
     project.resources :defects
     project.resources :developer_stats
-    project.resources :scrumboard
     project.resources :sprints do |sprint|
       sprint.resources :commitments, :collection => {:bulk_update => :post}
       sprint.resources :histories, :controller => :sprint_histories
+      sprint.resources :burndowns
+      sprint.resources :scrumboards
     end
   end
 end
