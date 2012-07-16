567dbe541113a48085e52ad8f919b8c69fe96a1c
Michael Cowden <mikecowden@razoo.com>
2012-07-16 16:47:22 -0400

    include reopens

--- app/controllers/burndowns_controller.rb
+++ app/controllers/burndowns_controller.rb
@@ -5,9 +5,9 @@ class BurndownsController < RedmineScrumController
 
   def index
     @burndown = BurndownFlot.area('Burndown') do |f|
-      f.series_for("Open", @sprint.burndown, :x => :sprint_day, :y => :open, :tooltip => lambda {|r| "#{r.open} points" })
-      f.series_for("Pending", @sprint.burndown, :x => :sprint_day, :y => :pending, :tooltip => lambda {|r| r.pending == 0 ? "#{r.open} points" : "#{r.pending} points" })
-      f.series_for("Reopens", @sprint.burndown, :x => :sprint_day, :y => :display_reopens, :options => {:points => {:show => true}, :lines => {:show => false}})
+      f.series_for("Open", @sprint.burndown, :x => :sprint_day, :y => :open, :tooltip => lambda {|r| "#{r.open} points open" })
+      f.series_for("Pending", @sprint.burndown, :x => :sprint_day, :y => :pending, :tooltip => lambda {|r| r.pending == 0 ? "#{r.open} points open" : "#{r.pending} points pending" })
+      f.series_for("Reopens", @sprint.burndown, :x => :sprint_day, :y => :display_reopens, :tooltip => lambda {|r| "#{r.display_reopens} stories reopened" }, :options => {:points => {:show => true}, :lines => {:show => false}})
     end
   end
 end
