55fe6048f5bd6a8aab450dd7a38ca8e562defd8d
Michael Cowden <mikecowden@razoo.com>
2012-07-16 16:49:18 -0400

    use standard label

--- app/controllers/burndowns_controller.rb
+++ app/controllers/burndowns_controller.rb
@@ -7,7 +7,7 @@ class BurndownsController < RedmineScrumController
     @burndown = BurndownFlot.area('Burndown') do |f|
       f.series_for("Open", @sprint.burndown, :x => :sprint_day, :y => :open, :tooltip => lambda {|r| "#{r.open} points open" })
       f.series_for("Pending", @sprint.burndown, :x => :sprint_day, :y => :pending, :tooltip => lambda {|r| r.pending == 0 ? "#{r.open} points open" : "#{r.pending} points pending" })
-      f.series_for("Reopens", @sprint.burndown, :x => :sprint_day, :y => :display_reopens, :tooltip => lambda {|r| "#{r.display_reopens} stories reopened" }, :options => {:points => {:show => true}, :lines => {:show => false}})
+      f.series_for("Reopens", @sprint.burndown, :x => :sprint_day, :y => :display_reopens, :options => {:points => {:show => true}, :lines => {:show => false}})
     end
   end
 end
