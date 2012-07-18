800fde015c2572f4c9bf5c727bd1c6a8e7ed4c31
Michael Cowden <mcowden@yahoo.com>
2012-07-17 20:08:27 -0400

    burndown json

--- app/controllers/burndowns_controller.rb
+++ app/controllers/burndowns_controller.rb
@@ -4,18 +4,17 @@ class BurndownsController < RedmineScrumController
   before_filter :setup_sprints
 
   def index
+    @burndown = BurndownFlot.area(@sprint) do |f|
+      f.series_for("Open", @sprint.burndown, :x => :sprint_day, :y => :open, :tooltip => lambda {|r| "#{r.open} points open" })
+      f.series_for("Pending", @sprint.burndown, :x => :sprint_day, :y => :pending, :tooltip => lambda {|r| r.pending == 0 ? "#{r.open} points open" : "#{r.pending} points pending" })
+      f.series_for("Reopens", @sprint.burndown, :x => :sprint_day, :y => :display_reopens, :options => {:points => {:show => true}, :lines => {:show => false}})
+    end
+
     respond_to do |format|
       format.html do
-        
-        @burndown = BurndownFlot.area(@sprint) do |f|
-          f.series_for("Open", @sprint.burndown, :x => :sprint_day, :y => :open, :tooltip => lambda {|r| "#{r.open} points open" })
-          f.series_for("Pending", @sprint.burndown, :x => :sprint_day, :y => :pending, :tooltip => lambda {|r| r.pending == 0 ? "#{r.open} points open" : "#{r.pending} points pending" })
-          f.series_for("Reopens", @sprint.burndown, :x => :sprint_day, :y => :display_reopens, :options => {:points => {:show => true}, :lines => {:show => false}})
-        end
-
       end
       format.json do 
-        render :json => @sprint.burndown
+        render :json => @burndown
       end
     end
   end
