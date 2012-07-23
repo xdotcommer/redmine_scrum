320636221f9eb3aa58dce0d932e7a4250c57175e
Michael Cowden <mikecowden@razoo.com>
2012-07-23 16:03:07 -0400

    sprint averages

--- app/controllers/sprint_average_controller.rb
+++ app/controllers/sprint_average_controller.rb
@@ -2,9 +2,6 @@ class SprintAverageController < RedmineScrumController
   unloadable
 
   def index
-
-    raise SprintAverage.new(params[:since] || 3.months.ago).to_json
-
     respond_to do |format|
       format.json do
         render :json => SprintAverage.new(params[:since] || 3.months.ago).to_json
