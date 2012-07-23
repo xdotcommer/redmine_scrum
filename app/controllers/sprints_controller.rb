8dbe27259458cd033b62aff9d10047fe8ca90b0e
Michael Cowden <mikecowden@razoo.com>
2012-07-23 18:44:46 -0400

    sprints as a chart

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -17,7 +17,7 @@ class SprintsController < RedmineScrumController
     respond_to do |wants|
       wants.html
       wants.json do
-        @sprints_data = DeveloperStatFlot.bars('sprints') do |f|
+        @sprints_data = DeveloperStatFlot.bar('sprints') do |f|
           f.series_for("Completed Points", @sprints, :x => :id, :y => :completed_points)
         end
 
