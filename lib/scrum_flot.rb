517bd6284572e80ff8eea340b8d1ee63ee5ed919
Michael Cowden <mikecowden@razoo.com>
2012-07-24 11:26:13 -0400

    sprints as a chart

--- lib/scrum_flot.rb
+++ lib/scrum_flot.rb
@@ -11,7 +11,13 @@ module ScrumFlot
     flot.options[:series] = {:stack => 0, :lines => {:show => true, :fill => true, :points => true}}
     flot
   end
-  
+
+  def bar(canvas = nil, &block)
+    flot = new(canvas, &block)
+    flot.options[:series] = {:bars => {:show => true, :barWidth => 0.5, :align => "center"}}
+    flot
+  end
+
   def stacked_bar(canvas = nil, &block)
     flot = new(canvas, &block)
     flot.options[:series] = {:stack => true, :bars => {:show => true, :barWidth => 0.5, :align => "center", :stack => true}}
