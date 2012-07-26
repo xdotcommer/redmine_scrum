f699f27a0a4f4530cbeb484c2e727a7f5897a5bb
Michael Cowden <mikecowden@razoo.com>
2012-07-26 15:14:12 -0400

    developer stats in json

--- app/controllers/developer_stats_controller.rb
+++ app/controllers/developer_stats_controller.rb
@@ -2,10 +2,10 @@ class DeveloperStatsController < RedmineScrumController
   unloadable
 
   def index
-    dev_exclude_list = ['Development Team', 'Will Schneider', 'Dan Hensgen', 'Stephen McGarrigle', 'Matt Watier']
+    dev_exclude_list = "'Development Team', 'Will Schneider', 'Dan Hensgen', 'Stephen McGarrigle', 'Matt Watier'"
     since = params[:since].blank? ? 3.months.ago.to_date : Date.parse(params[:since])
 
-    @stats = DeveloperStat.all(:include => [:sprint], :conditions => ["user_name not in (#{dev_exclude_list.join(", ")}) AND sprints.end_date < ? AND sprints.start_date > ?", 2.weeks.from_now.to_date, since], :order => 'sprint_name DESC, user_name ASC')
+    @stats = DeveloperStat.all(:include => [:sprint], :conditions => ["user_name NOT IN (#{dev_exclude_list}) AND sprints.end_date < ? AND sprints.start_date > ?", 2.weeks.from_now.to_date, since], :order => 'sprint_name DESC, user_name ASC')
 
     @completed_points = DeveloperStatFlot.area('completed_points') do |f|
       f.legend :position => "nw", :noColumns => 2
