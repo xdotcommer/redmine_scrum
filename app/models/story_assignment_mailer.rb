9ab59af2108d8b3ee2e1948ac0a20aa7ba9dc731
Michael Cowden <mikecowden@razoo.com>
2012-07-27 11:45:34 -0400

    trying the whole email on assignment thing

--- app/models/story_assignment_mailer.rb
+++ app/models/story_assignment_mailer.rb
@@ -7,7 +7,7 @@ class StoryAssignmentMailer < Mailer #ActionMailer::Base
     recipients "dev@razoo.com"
     subject "#{issue.assigned_to.try(:name)} has grabbed #{issue.tracker.try(:name)} ##{issue.id}: #{issue.subject}"
     body    :issue => issue,
-            :issue_url => url_for(:controller => 'issues', :action => 'show', :id => issue),
+            :issue_url => url_for(:controller => 'issues', :action => 'show', :id => issue)
     content_type "multipart/alternative"
 
     part "text/plain" do |p|
