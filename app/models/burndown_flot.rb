e96a4dc99ade26bd1fd57a7c576e697d21d85c8f
Michael Cowden <mikecowden@razoo.com>
2012-08-24 16:31:49 -0400

    fix burndown duration

--- app/models/burndown_flot.rb
+++ app/models/burndown_flot.rb
@@ -3,7 +3,7 @@ class BurndownFlot < Flot
 
   def initialize(sprint,  &block)
     @options ||= {}
-    xaxis :min => 0, :ticks => sprint.day_labels, :max => 10
+    xaxis :min => 0, :ticks => sprint.day_labels, :max => sprint.duration
     yaxis :min => 0, :tickDecimals => 0
     grid :hoverable => true
     super("Burndown", {}, &block)
